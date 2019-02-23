9c7b86034d95210f510efc8ea1a787112d1e334b
manish <manish.garg@wisethink.in>
2019-02-23 12:04:13 +0530

    Add project files.

---
+++ MIS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS.Startup))]
+namespace MIS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
